9aa02a0444e1294a99feca6830841732b3bb025c
Dennis Schulmeister-Zimolong <dennis@patk.org>
2018-09-27 01:32:39 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import MiniTutorial from "mini-tutorial.js";
+import mtThemeWhite from "mini-tutorial.js/themes/white.css";
+import mtThemeBook from "mini-tutorial.js/themes/book.css";
+import mtThemeCommon from "mini-tutorial.js/themes/common.css";
+import myStylesheet from "./style.css";
+
+window.addEventListener("load", () => {
+    let mt = new MiniTutorial();
+    mt.start();
+});
